ceadbf333f80ce0cb4f43a32fa349b553ca289d9
rajeswar-hash <rajeswarbind39@gmail.com>
2026-04-03 06:39:44 +0530

    Restore app auth provider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <App />
+    <Toaster richColors position="top-right" />
+  </AuthProvider>
+);
