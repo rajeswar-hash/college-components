def958b4548726ada176193f49b9d0a21b9c2caa
rajeswar-hash <rajeswarbind39@gmail.com>
2026-04-05 10:42:21 +0530

    Reduce homepage hero height on mobile

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -479,7 +479,7 @@ const Index = () => {
                 <div className="absolute -right-8 top-8 h-28 w-28 rounded-full bg-sky-400/10 blur-2xl" />
               </div>
 
-              <div className="relative min-h-[420px] sm:min-h-[500px]">
+              <div className="relative min-h-[380px] sm:min-h-[500px]">
                 <Card className="relative mx-auto mt-[62px] max-w-[332px] border-white/90 bg-white shadow-[0_22px_50px_rgba(15,23,42,0.16)] sm:mt-[108px] sm:max-w-[430px]">
                   <CardContent className="space-y-4 px-4 py-5 text-center sm:px-5 sm:py-6">
                     <div className="space-y-1.5">
