2a57dc1efdfbf15f0fa27763f893f29759e50d24
rajeswar-hash <rajeswarbind39@gmail.com>
2026-04-06 22:20:39 +0530

    Add desktop close button for toasts

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { Toaster } from "sonner";
 
+function ResponsiveToaster() {
+  const [showCloseButton, setShowCloseButton] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+    const sync = () => setShowCloseButton(mediaQuery.matches);
+
+    sync();
+    mediaQuery.addEventListener("change", sync);
+    return () => mediaQuery.removeEventListener("change", sync);
+  }, []);
+
+  return <Toaster richColors position="top-right" closeButton={showCloseButton} />;
+}
+
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
     <App />
-    <Toaster richColors position="top-right" />
+    <ResponsiveToaster />
   </AuthProvider>
 );
